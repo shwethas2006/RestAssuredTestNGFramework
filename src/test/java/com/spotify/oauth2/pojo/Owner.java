809e89eb62c792c72b465fcd5353ab8d03f417b1
shwethas2006 <shwethas2006>
2024-03-26 16:34:34 +0000

    Code re-factoring using lombok

--- src/test/java/com/spotify/oauth2/pojo/Owner.java
+++ src/test/java/com/spotify/oauth2/pojo/Owner.java
@@ -4,17 +4,11 @@ package com.spotify.oauth2.pojo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "display_name",
-    "external_urls",
-    "href",
-    "id",
-    "type",
-    "uri"
-})
-
+@Getter@Setter
 public class Owner {
 
     @JsonProperty("display_name")
@@ -30,64 +24,5 @@ public class Owner {
     @JsonProperty("uri")
     private String uri;
 
-    @JsonProperty("display_name")
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    @JsonProperty("display_name")
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @JsonProperty("external_urls")
-    public ExternalUrls__1 getExternalUrls() {
-        return externalUrls;
-    }
-
-    @JsonProperty("external_urls")
-    public void setExternalUrls(ExternalUrls__1 externalUrls) {
-        this.externalUrls = externalUrls;
-    }
-
-    @JsonProperty("href")
-    public String getHref() {
-        return href;
-    }
-
-    @JsonProperty("href")
-    public void setHref(String href) {
-        this.href = href;
-    }
-
-    @JsonProperty("id")
-    public String getId() {
-        return id;
-    }
-
-    @JsonProperty("id")
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @JsonProperty("type")
-    public String getType() {
-        return type;
-    }
-
-    @JsonProperty("type")
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    @JsonProperty("uri")
-    public String getUri() {
-        return uri;
-    }
-
-    @JsonProperty("uri")
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
 
 }
